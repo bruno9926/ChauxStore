9a768dd0ff104729cb5317fd749c3402fe8830ee
Bruno <david.lopez9926@gmail.com>
2025-05-20 01:28:41 -0500

    product slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,92 @@
-import Product from "../components/product/product";
+import Slider from "../components/slider/slider";
 
 export default function Home() {
 
   const products = [
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
+    {
+      image: "/images/products/blue flame.png",
+      title: "Distortion Blue Flame",
+      subtitle: "Oversized T-Shirt",
+      price: 4999,
+      isFavorite: true,
+    },
+    {
+      image: "/images/products/purple bomber.png",
+      title: "Purple Bomber Flame",
+      subtitle: "Bomber Jacket",
+      price: 4999,
+      isFavorite: false,
+    },
     {
       image: "/images/products/blue flame.png",
       title: "Distortion Blue Flame",
@@ -22,18 +106,7 @@ export default function Home() {
     <div>
       <h1>Welcome to the Store</h1>
       <h2>Featured Products</h2>
-      <div className="products">
-        {products.map((product, index) => (
-          <Product
-            key={index}
-            image={product.image}
-            title={product.title}
-            subtitle={product.subtitle}
-            price={product.price}
-            isFavorite={product.isFavorite}
-          />
-        ))}
-      </div>
+      <Slider products={products} />
     </div>
   );
 }
---
+++ src/components/slider/slider.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useRef } from 'react';
+import Product from '../product/product';
+import styles from './slider.module.scss';
+import { GoArrowRight, GoArrowLeft } from "react-icons/go";   
+
+export default function Slider({ products }: { products: any[] }) {
+
+    enum Direction {
+        LEFT = 'left',
+        RIGHT = 'right',
+    }
+
+    const sliderRef = useRef<HTMLDivElement>(null);
+
+    const scroll = (direction: Direction) => {
+        const container = sliderRef.current;
+        if (!container) return;
+        const scrollAmount = 700;
+        container.scrollBy({
+            left: direction === Direction.LEFT ? -scrollAmount : scrollAmount,
+            behavior: 'smooth',
+        });
+    }    
+
+    const Arrow = ({ direction }: { direction: Direction }) => {
+        const directionStyles = direction === Direction.LEFT ? styles.left : styles.right;
+
+        return (
+            <div className={`${styles.arrow} ${directionStyles}`} onClick={() => scroll(direction)}>
+                {direction === Direction.LEFT ? <GoArrowLeft /> : <GoArrowRight />}
+            </div>
+        );
+    }
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.slider} ref={sliderRef}>
+            {products.map((product, index) => (
+                <Product
+                    key={index}
+                    image={product.image}
+                    title={product.title}
+                    subtitle={product.subtitle}
+                    price={product.price}
+                    isFavorite={product.isFavorite}
+                />
+            ))}
+            </div>
+            <Arrow direction={Direction.LEFT} />    
+            <Arrow direction={Direction.RIGHT} />
+        </div>
+    )
+}
